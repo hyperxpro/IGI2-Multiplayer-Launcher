cfbe8c559aafe1bce849b7f4d2e839ae074546a8
Aayush Atharva <hyperx.pro@outlook.com>
2018-01-27 16:22:15 +0530

    Add files via upload

--- src/aayush/atharva/igi2/multiplayer/launcher/anticheat/MainCoreSystem/AntiCheatImplement.java
+++ src/aayush/atharva/igi2/multiplayer/launcher/anticheat/MainCoreSystem/AntiCheatImplement.java
@@ -1,3 +1,21 @@
+/*
+ * IGI-2 Multiplayer Launcher
+ *
+ * Copyright (c) 2018, Aayush Atharva
+ *
+ * Permission is hereby granted, free of charge to any person obtaining a copy of this software and associated documentation files 
+ * (the "Software"), to deal in the Software with restriction. A person can use, copy the Software without restriction. But if a person 
+ * modify the software, the person must push the code to the Software GitHub repository. If a person wants to publish or distribute the 
+ * software, the person must put this "Created By: Aayush Atharva" on About Section of the Software And this License must be present with 
+ * every file of the Software. And If a person wants to sell the software, the person get permission from the owner of this Software.
+ *
+ *
+ *
+ *
+ * IGI-2 Multiplayer Launcher
+ * Owner: Aayush Atharva
+ * Email: aayush@igi2.co.in
+ */
 package aayush.atharva.igi2.multiplayer.launcher.anticheat.MainCoreSystem;
 
 import aayush.atharva.igi2.multiplayer.launcher.anticheat.algorithm.filesverification.maps.*;
