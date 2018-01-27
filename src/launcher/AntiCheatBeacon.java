fe9fcb50881c27c0b58a5ccdcc39920118037ac2
Aayush Atharva <hyperx.pro@outlook.com>
2018-01-27 15:33:12 +0530

    Update AntiCheatBeacon.java

--- src/launcher/AntiCheatBeacon.java
+++ src/launcher/AntiCheatBeacon.java
@@ -1,3 +1,20 @@
+/*
+IGI-2 Multiplayer Launcher
+
+Copyright (c) 2018, Aayush Atharva
+
+Permission is hereby granted, free of charge to any person obtaining a copy of this software and associated documentation files 
+(the "Software"), to deal in the Software with restriction. A person can use, copy the Software without restriction. But if a person modify
+the software, the person must push the code to the Software GitHub repository. If a person wants to publish or distribute the software,
+the person must put this "Created By: Aayush Atharva" on About Section of the Software. And If a person want's to sell the software,
+the person get permission from the owner of this Software. 
+
+
+IGI-2 Multiplayer Launche
+Owner: Aayush Atharva
+Email: aayush@igi2.co.in
+*/
+
 package launcher;
 
 import static aayush.atharva.igi2.multiplayer.launcher.anticheat.MainCoreSystem.AntiCheat_Execute.BridgeDentsrSHA_1;
