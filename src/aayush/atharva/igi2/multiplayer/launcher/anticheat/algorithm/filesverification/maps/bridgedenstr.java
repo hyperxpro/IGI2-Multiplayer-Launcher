c1fd6495711b21a62240aeb4ecf0510002c9a646
Aayush Atharva <hyperx.pro@outlook.com>
2019-02-12 15:52:44 +0530

    License Change

--- src/aayush/atharva/igi2/multiplayer/launcher/anticheat/algorithm/filesverification/maps/bridgedenstr.java
+++ src/aayush/atharva/igi2/multiplayer/launcher/anticheat/algorithm/filesverification/maps/bridgedenstr.java
@@ -1,20 +1,57 @@
 /*
  * IGI-2 Multiplayer Launcher
+ * ________________________________________
+ * End-User License Agreement ("Agreement")
  *
- * Copyright (c) 2018, Aayush Atharva
+ * Last updated: 12-February-2019
  *
- * Permission is hereby granted, free of charge to any person obtaining a copy of this software and associated documentation files 
- * (the "Software"), to deal in the Software with restriction. A person can use, copy the Software without restriction. But if a person 
- * modify the software, the person must push the code to the Software GitHub repository. If a person wants to publish or distribute the 
- * software, the person must put this "Created By: Aayush Atharva" on About Section of the Software And this License must be present with 
- * every file of the Software. And If a person wants to sell the software, the person get permission from the owner of this Software.
+ * Please read this End-User License Agreement ("Agreement") carefully before clicking the "I Agree" button, downloading or using IGI-2 Multiplayer Launcher ("Application").
+ *
+ * By clicking the "I Agree" button, downloading or using the Application, you are agreeing to be bound by the terms and conditions of this Agreement.
+ *
+ * If you do not agree to the terms of this Agreement, do not click on the "I Agree" button and do not download or use the Application.
+ *
+ * License
+ *
+ * Aayush Atharva grants you a revocable, non-exclusive, non-transferable, limited license to download, install, modify and use the Application solely for your personal, non-commercial purposes strictly in accordance with the terms of this Agreement. 
+ *
+ * Restrictions
+ *
+ * You agree not to, and you will not permit others to:
+ *
+ * a) license, sell, rent, lease, assign, distribute, transmit, host, outsource, disclose or otherwise commercially exploit the Application or make the Application available to any third party.
  *
  *
+ * Modifications to Application
+ *
+ * Aayush Atharva reserves the right to modify, suspend or discontinue, temporarily or permanently, the Application or any service to which it connects, with or without notice and without liability to you.
  *
  *
- * IGI-2 Multiplayer Launcher
- * Owner: Aayush Atharva
- * Email: aayush@igi2.co.in
+ * Term and Termination
+ *
+ * This Agreement shall remain in effect until terminated by you or Aayush Atharva.
+ *
+ * Aayush Atharva may, in its sole discretion, at any time and for any or no reason, suspend or terminate this Agreement with or without prior notice.
+ *
+ * This Agreement will terminate immediately, without prior notice from Aayush Atharva, in the event that you fail to comply with any provision of this Agreement. You may also terminate this Agreement by deleting the Application and all copies thereof from any device where itÃ¢â‚¬â„¢s stored.
+ *
+ * Upon termination of this Agreement, you shall cease all use of the Application and delete all copies of the Application from any device where itÃ¢â‚¬â„¢s stored.
+ *
+ * Severability
+ *
+ * If any provision of this Agreement is held to be unenforceable or invalid, such provision will be changed and interpreted to accomplish the objectives of such provision to the greatest extent possible under applicable law and the remaining provisions will continue in full force and effect.
+ *
+ * Amendments to this Agreement
+ *
+ * Aayush Atharva reserves the right, at its sole discretion, to modify or replace this Agreement at any time. If a revision is material I will provide at least 7 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.
+ *
+ * Contact Information
+ *
+ * If you have any questions about this Agreement, please contact me.
+ *
+ *
+ * Aayush Atharva
+ * Email: aayush@aayushatharva.com
  */
 package aayush.atharva.igi2.multiplayer.launcher.anticheat.algorithm.filesverification.maps;
 
